98a97e7cbeec37650e78ab481bd09a4232c64ca3
Kerioney <fareselkerioney@gmail.com>
2023-03-25 03:51:17 +0200

    Drug Controllers

--- Controllers/drug.controllers.js
+++ Controllers/drug.controllers.js
@@ -47,14 +47,77 @@ let getDrug = async (req, res, next) => {
         next(err)
     }
 }
+let similarDrugs = async (req, res, next) => {
+    try {
+        const currentPage = req.query.page || 1
+        const perPage = req.query.perPage || 6
+        let totalDrugs
+        let drug = await Drug.findById(req.params.id)
+        const count = await Drug.find({
+            activeIngredient: drug.activeIngredient,
+        }).countDocuments()
+        totalDrugs = count
+        let similarDrugs = await Drug.find({
+            activeIngredient: drug.activeIngredient,
+        })
+            .skip((currentPage - 1) * perPage)
+            .limit(perPage)
+            // i want to sort the drugs by price ascending
+            .sort({ price: 1 })
+        res.status(200).json({
+            message: "Fetched similar drugs successfully.",
+            similarDrugs,
+            totalDrugs,
+        })
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err)
+    }
+}
 
-// let getAllSimilarDrugs = (req, res) => {
-//     Drug.find({ activeIngredient: req.params.activeIngredient })
-//         .then((drugs) => res.send(drugs))
-//         .catch((err) => console.log(err))
-// }
+let drugSearch = async (req, res, next) => {
+    const search = req.query.search
+    const currentPage = req.query.page || 1
+    const perPage = req.query.perPage || 6
+    let totalDrugs
+    try {
+        const count = await Drug.find({
+            name: { $regex: search, $options: "i" },
+        }).countDocuments() // count the number of drugs
+        totalDrugs = count
 
+        const drugs = await Drug.find({
+            name: { $regex: "^" + search, $options: "i" },
+        })
+            .select(" -category") // select the name and activeIngredient fields
+            // pagination
+            .skip((currentPage - 1) * perPage)
+            .limit(perPage)
+
+        if (!drugs) {
+            const error = new Error("Could not find any drug")
+            error.statusCode = 404
+            throw error
+        } else {
+            res.status(200).json({
+                message: "Fetched drugs successfully.",
+
+                drugs,
+                totalDrugs,
+            })
+        }
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err)
+    }
+}
 module.exports = {
     getAllDrugs,
     getDrug,
+    similarDrugs,
+    drugSearch,
 }
--- Routes/drug.routes.js
+++ Routes/drug.routes.js
@@ -3,5 +3,7 @@ const drugController = require("../Controllers/drug.controllers")
 
 route.get("/drugs", drugController.getAllDrugs)
 route.get("/drug/:id", drugController.getDrug)
+route.get("/similarDrugs/:id", drugController.similarDrugs)
+route.get("/search", drugController.drugSearch)
 
 module.exports = route
